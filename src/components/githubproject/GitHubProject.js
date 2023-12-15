5ac0df13045ad57612dcd6e45e2a519fd51b63f9
sangram chopdar <sangramdadasochopdar@gmail.com>
2023-12-15 23:13:07 +0530

    loading component is added

--- src/components/githubproject/GitHubProject.js
+++ src/components/githubproject/GitHubProject.js
@@ -1,20 +1,29 @@
 import React, { useState, useEffect } from "react";
+import Loading from "./Loading";
 
 const GitHubProject = () => {
   const [users, setUsers] = useState([]);
+  const [loading, setLoaing] = useState(true);
 
   const getUser = async () => {
     try {
       const res = await fetch("https://api.github.com/users");
+      setLoaing(true);
       const data = await res.json();
       setUsers(data);
+      setLoaing(false);
     } catch (error) {
+      setLoaing(true);
       console.error("Error fetching users:", error);
     }
   };
   useEffect(() => {
     getUser();
   }, []);
+
+  if (loading) {
+    return <Loading />;
+  }
 
   return (
     <div className="container mt-5">
@@ -28,25 +37,25 @@ const GitHubProject = () => {
                 alt={user.login}
               />
               <div className="card-body">
-                <h5 className="card-title">Usename:{user.login}</h5>
-                <p className="card-text">Type:{user.type}</p>
+                <h5 className="card-title">Username: {user.login}</h5>
+                <p className="card-text">Type: {user.type}</p>
                 <ul className="list-group list-group-flush">
                   <li className="list-group-item">
                     <b>Public Repos: </b>
                     <a href={user.repos_url} className="highlight-link">
-                      {user.repos_url}
+                      See Public Repos
                     </a>
                   </li>
                   <li className="list-group-item">
                     <b>Followers: </b>
                     <a href={user.followers_url} className="highlight-link">
-                      {user.followers_url}
+                      See Followers
                     </a>
                   </li>
                   <li className="list-group-item">
                     <b>Following: </b>{" "}
                     <a href={user.following_url} className="highlight-link">
-                      {user.following_url}
+                      See Following
                     </a>
                   </li>
                 </ul>
